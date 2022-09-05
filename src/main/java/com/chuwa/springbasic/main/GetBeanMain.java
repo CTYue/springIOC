bd0fd9263ee74c1b87b028ff22de82721d03b244
TAIsRich <Richard.lyu0120@gmail.com>
2022-09-05 00:43:07 -0700

    add getBean example

---
+++ src/main/java/com/chuwa/springbasic/main/GetBeanMain.java
@@ -0,0 +1,41 @@
+package com.chuwa.springbasic.main;
+
+import com.chuwa.springbasic.config.BeanConfig;
+import com.chuwa.springbasic.service.DependencyInjectionByTypeByName;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @author b1go
+ * @date 9/5/22 12:35 AM
+ */
+public class GetBeanMain {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfig.class);
+
+        /**
+         * 1. Retrieving Bean by Name and Type
+         * 2. Retrieving Bean by Name
+         * 3. Retrieving Bean by Type
+         */
+
+        System.out.println("\n ******  1. Retrieving Bean by Name and Type *****");
+        // 1. Retrieving Bean by Name and Type
+        context.getBean("dependencyInjectionByTypeByName", DependencyInjectionByTypeByName.class).printFirstMessage();
+
+        System.out.println("\n ******  2. Retrieving Bean by Name *****");
+        // 2. Retrieving Bean by Name
+        Object o = context.getBean("dependencyInjectionByTypeByName");
+        DependencyInjectionByTypeByName di = (DependencyInjectionByTypeByName) o;
+        di.printFirstMessage();
+
+        System.out.println("\n ******  or *****");
+        // or
+        ((DependencyInjectionByTypeByName) context.getBean("dependencyInjectionByTypeByName")).printFirstMessage();
+
+        // 3. Retrieving Bean by Type
+        System.out.println("\n ******  3. Retrieving Bean by Type *****");
+        context.getBean(DependencyInjectionByTypeByName.class).printFirstMessage();
+    }
+}
